ef11954ff35e3560b69cf9eecfddcab616c6cacc
N.SH <sky.blush@ya.ru>
2022-05-11 20:22:41 +0300

    add searchForm

---
+++ src/components/SearchForm/SearchForm.js
@@ -0,0 +1,20 @@
+import './SearchForm.css'
+
+function SearchForm() {
+  return (
+    <section className="search">
+      <form className='search__form'>
+        <div className='search__field'>
+          <input className="search__input" type="text" name='movie' placeholder="Фильм" required />
+          <button className='search__button' type='submit'>Найти</button>
+        </div>
+        <div className='search__checkbox-container'>
+          <input className='search__checkbox' type='checkbox' name='shortFilms' />
+          <label className='search__checkbox-label'>Короткометражки</label>
+        </div>
+      </form>
+    </section>
+  )
+}
+
+export default SearchForm
