92f13c7608d03949b0930067116925cd84e5026f
N.SH <sky.blush@ya.ru>
2022-05-14 14:50:58 +0300

    add register form

---
+++ src/components/Register/Register.js
@@ -0,0 +1,35 @@
+import logo from '../../images/logo.svg'
+import { Link } from 'react-router-dom'
+import './Register.css'
+
+function Register() {
+  return (
+    <section className="register">
+      <img className="register__logo" src={logo} alt="Логотип сайта" />
+      <h2 className="register__title">Добро пожаловать!</h2>
+      <form className="register__form">
+        <div className="register__field">
+          <label className="register__label">Имя</label>
+          <input className="register__input" type="text" name="name" placeholder="Имя" required />
+          <span className="register__input-error"></span>
+        </div>
+        <div className="register__field">
+          <label className="register__label">E-mail</label>
+          <input className="register__input" type="email" name="email" placeholder="E-mail" required />
+          <span className="register__input-error"></span>
+        </div>
+        <div className="register__field">
+          <label className="register__label">Пароль</label>
+          <input className="register__input" type="password" name="password" placeholder="Пароль" required minLength="6" />
+          <span className="register__input-error">Что-то пошло не так...</span>
+        </div>
+        <button className="register__button" type="submit">Зарегистрироваться</button>
+        <p className="register__login-sign">Уже зарегистрированы?&nbsp;
+          <Link to="/sign-in" className="register__login-link">Войти</Link>
+        </p>
+      </form>
+    </section>
+  )
+}
+
+export default Register
