be74f01abdcafb0f930ea225ec92cb08746f4dc3
N.SH <sky.blush@ya.ru>
2022-05-16 16:29:42 +0300

    update SearchForm

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -3,14 +3,14 @@ import './SearchForm.css'
 function SearchForm() {
   return (
     <section className="search">
-      <form className='search__form'>
-        <div className='search__field'>
-          <input className="search__input" type="text" name='movie' placeholder="Фильм" required />
-          <button className='search__button hover-button' type='submit'>Найти</button>
+      <form className="search__form">
+        <div className="search__field">
+          <input className="search__input" type="text" name="movie" placeholder="Фильм" required />
+          <button className="search__button hover-button" type="submit">Найти</button>
         </div>
-        <div className='search__checkbox-container'>
-          <input className='search__checkbox' type='checkbox' name='shortFilms' />
-          <label className='search__checkbox-label'>Короткометражки</label>
+        <div className="search__checkbox-container">
+          <input className="search__checkbox" type="checkbox" name="shortFilms" />
+          <label className="search__checkbox-label">Короткометражки</label>
         </div>
       </form>
     </section>
