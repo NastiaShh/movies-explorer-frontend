33021e3e16a90022aa1243293a99146f68296932
N.SH <sky.blush@ya.ru>
2022-05-16 16:31:09 +0300

    add routes and components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,42 @@
-import '../../index.css'
+import { Switch, Route } from 'react-router-dom'
 import Header from '../Header/Header'
 import Main from '../Main/Main'
+import Movies from '../Movies/Movies'
+import SavedMovies from '../SavedMovies/SavedMovies'
+import Register from '../Register/Register'
+import Login from '../Login/Login'
+import Profile from '../Profile/Profile'
 import Footer from '../Footer/Footer'
+import PageNotFound from '../PageNotFound/PageNotFound'
+import '../../index.css'
 
 function App() {
   return (
     <>
       <Header />
-      <Main />
+      <Switch>
+        <Route exact path="/">
+          <Main />
+        </Route>
+        <Route path="/movies">
+          <Movies />
+        </Route>
+        <Route path="/saved-movies">
+          <SavedMovies />
+        </Route>
+        <Route path="/profile">
+          <Profile />
+        </Route>
+        <Route path="/signin">
+          <Login />
+        </Route>
+        <Route path="/signup">
+          <Register />
+        </Route>
+        <Route path="*">
+          <PageNotFound />
+        </Route>
+      </Switch>
       <Footer />
     </>
   )
