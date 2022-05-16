141918db50802c280a73cb0faa6932520cad0f41
N.SH <sky.blush@ya.ru>
2022-05-16 14:51:15 +0300

    add hamburger-menu, update Navigation component

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,12 +1,43 @@
-import { Link } from 'react-router-dom'
+import React from 'react'
+import { Switch, Route, Link, NavLink } from 'react-router-dom'
 import './Navigation.css'
 
 function Navigation() {
+  const [isPopupOpen, setIsPopupOpen] = React.useState(false)
+
+  function handlePopupOpen() {
+    setIsPopupOpen(true)
+  }
+
+  function handlePopupClose() {
+    setIsPopupOpen(false)
+  }
+
   return (
-    <div className="navigation">
-      <Link to="/sign-up" className="navigation__link hover-link">Регистрация</Link>
-      <Link to="/sign-in" className="navigation__link hover-button">Войти</Link>
-    </div>
+    <Switch>
+      <Route exact path="/">
+        <nav className="navigation">
+          <Link to="/sign-up" className="navigation__link navigation__link_unlogged hover-link">Регистрация</Link>
+          <Link to="/sign-in" className="navigation__link navigation__link_unlogged hover-button">Войти</Link>
+        </nav>
+      </Route>
+
+      <Route exact path="/movies">
+        <div className={`${isPopupOpen ? "overlay" : ""}`}>
+          <nav className={`navigation ${isPopupOpen ? "navigation__popup_open" : ""}`}>
+            <button className="navigation__popup-button navigation__popup-button_close hover-button" onClick={handlePopupClose}></button>
+            <Link to="/" className="navigation__link navigation__link_loggedin navigation__link-main hover-link">Главная</Link>
+            <NavLink to="/movies" className="navigation__link navigation__link_loggedin hover-link" activeClassName="navigation__link_active">Фильмы</NavLink>
+            <NavLink to="/saved-movies" className="navigation__link navigation__link_loggedin hover-link" activeClassName="navigation__link_active">Сохранённые фильмы</NavLink>
+            <Link to="/profile" className="navigation__account-button hover-button">
+              <div className="navigation__account-icon"></div>
+              Аккаунт
+            </Link>
+          </nav>
+        </div>
+        <button className="navigation__popup-button navigation__popup-button_burger hover-button" onClick={handlePopupOpen}></button>
+      </Route>
+    </Switch>
   )
 }
 
