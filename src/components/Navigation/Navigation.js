9a9a2bce70d7d8d7db2bf585c8040d737e0f223c
N.SH <sky.blush@ya.ru>
2022-06-04 23:32:33 +0300

    update Navigation component

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import { Switch, Route, Link, NavLink } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import './Navigation.css'
 
-function Navigation() {
+function Navigation(props) {
   const [isPopupOpen, setIsPopupOpen] = React.useState(false)
 
   function handlePopupOpen() {
@@ -14,30 +14,30 @@ function Navigation() {
   }
 
   return (
-    <Switch>
-      <Route exact path="/">
+    <>
+      {!props.loggedIn ? (
         <nav className="navigation">
           <Link to="/signup" className="navigation__link navigation__link_unlogged hover-link">Регистрация</Link>
           <Link to="/signin" className="navigation__link navigation__link_unlogged hover-button">Войти</Link>
         </nav>
-      </Route>
-
-      <Route exact path="/movies">
-        <div className={`${isPopupOpen ? "overlay" : ""}`}>
-          <nav className={`navigation navigation__popup ${isPopupOpen ? "navigation__popup_open" : ""}`}>
-            <button className="navigation__popup-button navigation__popup-button_close hover-button" onClick={handlePopupClose}></button>
-            <Link to="/" className="navigation__link navigation__link_loggedin navigation__link-main hover-link">Главная</Link>
-            <NavLink to="/movies" className="navigation__link navigation__link_loggedin hover-link" activeClassName="navigation__link_active">Фильмы</NavLink>
-            <NavLink to="/saved-movies" className="navigation__link navigation__link_loggedin hover-link" activeClassName="navigation__link_active">Сохранённые фильмы</NavLink>
-            <Link to="/profile" className="navigation__account-button hover-button">
-              <div className="navigation__account-icon"></div>
-              Аккаунт
-            </Link>
-          </nav>
-        </div>
-        <button className="navigation__popup-button navigation__popup-button_burger hover-button" onClick={handlePopupOpen}></button>
-      </Route>
-    </Switch>
+      ) : (
+        <>
+          <div className={`${isPopupOpen ? "overlay" : ""}`}>
+            <nav className={`navigation navigation__popup ${isPopupOpen ? "navigation__popup_open" : ""}`}>
+              <button className="navigation__popup-button navigation__popup-button_close hover-button" onClick={handlePopupClose}></button>
+              <Link to="/" className="navigation__link navigation__link_loggedin navigation__link-main hover-link">Главная</Link>
+              <NavLink to="/movies" className="navigation__link navigation__link_loggedin hover-link" activeClassName="navigation__link_active">Фильмы</NavLink>
+              <NavLink to="/saved-movies" className="navigation__link navigation__link_loggedin hover-link" activeClassName="navigation__link_active">Сохранённые фильмы</NavLink>
+              <Link to="/profile" className="navigation__account-button hover-button">
+                <div className="navigation__account-icon"></div>
+                Аккаунт
+              </Link>
+            </nav>
+          </div>
+          <button className="navigation__popup-button navigation__popup-button_burger hover-button" onClick={handlePopupOpen}></button>
+        </>
+      )}
+    </>
   )
 }
 
