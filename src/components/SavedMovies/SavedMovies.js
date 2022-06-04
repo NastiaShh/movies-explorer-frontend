1f31ea6188fadf9a415c260c796b232070879d68
N.SH <sky.blush@ya.ru>
2022-06-04 23:09:58 +0300

    add SavedMovies component

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -1,18 +1,52 @@
-import './SavedMovies.css'
-// import Header from '../Header/Header'
+import { useState, useEffect } from 'react'
 import SearchForm from '../SearchForm/SearchForm'
 import MoviesCardList from '../MoviesCardList/MoviesCardList'
-// import Preloader from '../Preloader/Preloader'
+import './SavedMovies.css'
 
-function SavedMovies() {
+function SavedMovies(props) {
+  const [filteredMovies, setFilteredMovies] = useState([])
+
+  function handleSearch(movieName, isShortFilms) {
+    const filteredMovies = props.cards.filter((item) => item.nameRU.toLowerCase().includes(movieName.toLowerCase()))
+    if (isShortFilms) {
+      setFilteredMovies(filteredMovies.filter((item) => item.duration <= 40))
+    }
+    else {
+      setFilteredMovies(filteredMovies)
+    }
+  }
+
+  function initFilteredMovies() {
+    setFilteredMovies(props.cards)
+  }
+
+  useEffect(() => {
+    setFilteredMovies(
+      filteredMovies.filter(movie => props.cards.some(card => movie.movieId === card.movieId))
+    )
+  }, [props.cards])
+
+  useEffect(() => {
+    initFilteredMovies()
+  }, [])
+
   return (
-    <main className="saved-movies">
-      {/* <Header /> */}
-      <SearchForm />
-      <MoviesCardList />
-      {/* <Preloader /> */}
-      <button className="saved-movies__more-button hover-button" type="button">Ещё</button>
-    </main>
+    <>
+      <SearchForm
+        handleSearch={handleSearch}
+        defaultValue=""
+      />
+      <main className="saved-movies">
+        <MoviesCardList
+          cards={filteredMovies}
+          isSaved={props.isSaved}
+          isOnlySaved={true}
+          onCardDelete={props.onCardDelete}
+          serverError={props.serverError}
+          loading={props.loading}
+        />
+      </main>
+    </>
   )
 }
 
