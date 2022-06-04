263ea13f89821e8337a66739a90d5d4aedb6d2de
N.SH <sky.blush@ya.ru>
2022-06-04 23:44:29 +0300

    add Auth api

---
+++ src/utils/Auth.js
@@ -0,0 +1,48 @@
+export const BASE_URL = process.env.REACT_APP_BASE_URL || 'https://localhost:3000'
+// для локальной отладки
+// export const BASE_URL = 'http://localhost:3001'
+// export const BASE_URL = 'https://api.movies-explorer.nsh.nomoredomains.work'
+
+const checkResponseStatus = (res) => {
+  if (res.ok) {
+    return res.json()
+  }
+  return Promise.reject(`Ошибка: ${res.status}`)
+}
+
+export const register = (name, email, password) => {
+  return fetch(`${BASE_URL}/signup`, {
+    method: 'POST',
+    credentials: 'include',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({name, email, password})
+  })
+  .then(checkResponseStatus)
+}
+
+export const login = (email, password) => {
+  return fetch(`${BASE_URL}/signin`, {
+    method: 'POST',
+    credentials: 'include',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({email, password})
+  })  
+}
+
+export const logout = () => {
+  return fetch(`${BASE_URL}/signout`, {
+    method: 'POST',
+    credentials: 'include',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+  })
+  .then(checkResponseStatus)
+}
