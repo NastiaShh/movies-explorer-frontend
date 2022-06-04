5794364f60952f26502450b7dd43a09e6e87b901
N.SH <sky.blush@ya.ru>
2022-06-04 23:26:26 +0300

    add Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,23 @@
+import { Route } from 'react-router-dom'
 import Logo from '../Logo/Logo'
 import Navigation from '../Navigation/Navigation'
 import './Header.css'
 
-function Header() {
+function Header(props) {
+  const endpoints = [
+    "/",
+    "/profile",
+    "/movies",
+    "/saved-movies",
+  ]
+
   return (
-    <header className="header">
-      <Logo />
-      <Navigation />
-    </header>
+    <Route exact path={endpoints}>
+      <header className="header">
+        <Logo />
+        <Navigation loggedIn={props.loggedIn} />
+      </header>
+    </Route>
   )
 }
 
