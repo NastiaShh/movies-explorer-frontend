2b42993c27989634ced878ab9fb98ebc16d13ffc
N.SH <sky.blush@ya.ru>
2022-06-05 18:30:41 +0300

    fix closing burger-menu after click on link

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -25,10 +25,10 @@ function Navigation(props) {
           <div className={`${isPopupOpen ? "overlay" : ""}`}>
             <nav className={`navigation navigation__popup ${isPopupOpen ? "navigation__popup_open" : ""}`}>
               <button className="navigation__popup-button navigation__popup-button_close hover-button" onClick={handlePopupClose}></button>
-              <Link to="/" className="navigation__link navigation__link_loggedin navigation__link-main hover-link">Главная</Link>
-              <NavLink to="/movies" className="navigation__link navigation__link_loggedin hover-link" activeClassName="navigation__link_active">Фильмы</NavLink>
-              <NavLink to="/saved-movies" className="navigation__link navigation__link_loggedin hover-link" activeClassName="navigation__link_active">Сохранённые фильмы</NavLink>
-              <Link to="/profile" className="navigation__account-button hover-button">
+              <Link to="/" className="navigation__link navigation__link_loggedin navigation__link-main hover-link" onClick={handlePopupClose}>Главная</Link>
+              <NavLink to="/movies" className="navigation__link navigation__link_loggedin hover-link" activeClassName="navigation__link_active" onClick={handlePopupClose}>Фильмы</NavLink>
+              <NavLink to="/saved-movies" className="navigation__link navigation__link_loggedin hover-link" activeClassName="navigation__link_active" onClick={handlePopupClose}>Сохранённые фильмы</NavLink>
+              <Link to="/profile" className="navigation__account-button hover-button" onClick={handlePopupClose}>
                 <div className="navigation__account-icon"></div>
                 Аккаунт
               </Link>
